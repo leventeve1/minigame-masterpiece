33fc3f92d25e8d4602b0e87628b3643f1a772e0c
leventeve1 <selleylevente@gmail.com>
2021-11-06 18:44:24 +0100

    back end: színezés, kezdetleges játék

--- minigame/minigame/Form1.Designer.cs
+++ minigame/minigame/Form1.Designer.cs
@@ -48,135 +48,149 @@ namespace minigame
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.button1.Location = new System.Drawing.Point(49, 43);
+            this.button1.Location = new System.Drawing.Point(37, 35);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 75);
+            this.button1.Size = new System.Drawing.Size(56, 61);
             this.button1.TabIndex = 0;
             this.button1.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
-            this.button2.Location = new System.Drawing.Point(130, 43);
+            this.button2.Location = new System.Drawing.Point(98, 35);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 75);
+            this.button2.Size = new System.Drawing.Size(56, 61);
             this.button2.TabIndex = 1;
             this.button2.UseVisualStyleBackColor = false;
             // 
             // button3
             // 
             this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.button3.Location = new System.Drawing.Point(211, 43);
+            this.button3.Location = new System.Drawing.Point(158, 35);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 75);
+            this.button3.Size = new System.Drawing.Size(56, 61);
             this.button3.TabIndex = 2;
             this.button3.UseVisualStyleBackColor = false;
             // 
             // button4
             // 
             this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.button4.Location = new System.Drawing.Point(49, 124);
+            this.button4.Location = new System.Drawing.Point(37, 101);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 75);
+            this.button4.Size = new System.Drawing.Size(56, 61);
             this.button4.TabIndex = 3;
             this.button4.UseVisualStyleBackColor = false;
             // 
             // button5
             // 
             this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.button5.Location = new System.Drawing.Point(130, 124);
+            this.button5.Location = new System.Drawing.Point(98, 101);
+            this.button5.Margin = new System.Windows.Forms.Padding(2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 75);
+            this.button5.Size = new System.Drawing.Size(56, 61);
             this.button5.TabIndex = 4;
             this.button5.UseVisualStyleBackColor = false;
             // 
             // button6
             // 
             this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.button6.Location = new System.Drawing.Point(211, 124);
+            this.button6.Location = new System.Drawing.Point(158, 101);
+            this.button6.Margin = new System.Windows.Forms.Padding(2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(75, 75);
+            this.button6.Size = new System.Drawing.Size(56, 61);
             this.button6.TabIndex = 5;
             this.button6.UseVisualStyleBackColor = false;
             // 
             // button7
             // 
             this.button7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.button7.Location = new System.Drawing.Point(49, 205);
+            this.button7.Location = new System.Drawing.Point(37, 167);
+            this.button7.Margin = new System.Windows.Forms.Padding(2);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(75, 75);
+            this.button7.Size = new System.Drawing.Size(56, 61);
             this.button7.TabIndex = 6;
             this.button7.UseVisualStyleBackColor = false;
             // 
             // button8
             // 
             this.button8.BackColor = System.Drawing.Color.Gray;
-            this.button8.Location = new System.Drawing.Point(130, 205);
+            this.button8.Location = new System.Drawing.Point(98, 167);
+            this.button8.Margin = new System.Windows.Forms.Padding(2);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(75, 75);
+            this.button8.Size = new System.Drawing.Size(56, 61);
             this.button8.TabIndex = 7;
             this.button8.UseVisualStyleBackColor = false;
             // 
             // button9
             // 
-            this.button9.BackColor = System.Drawing.Color.White;
-            this.button9.Location = new System.Drawing.Point(211, 205);
+            this.button9.BackColor = System.Drawing.Color.Silver;
+            this.button9.Location = new System.Drawing.Point(158, 167);
+            this.button9.Margin = new System.Windows.Forms.Padding(2);
             this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(75, 75);
+            this.button9.Size = new System.Drawing.Size(56, 61);
             this.button9.TabIndex = 8;
             this.button9.UseVisualStyleBackColor = false;
             // 
             // round
             // 
             this.round.AutoSize = true;
-            this.round.Location = new System.Drawing.Point(436, 43);
+            this.round.Location = new System.Drawing.Point(327, 35);
+            this.round.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.round.Name = "round";
-            this.round.Size = new System.Drawing.Size(54, 17);
+            this.round.Size = new System.Drawing.Size(42, 13);
             this.round.TabIndex = 9;
             this.round.Text = "Round:";
-            this.round.Click += new System.EventHandler(this.label1_Click);
             // 
             // high_score
             // 
             this.high_score.AutoSize = true;
-            this.high_score.Location = new System.Drawing.Point(436, 263);
+            this.high_score.Location = new System.Drawing.Point(327, 214);
+            this.high_score.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.high_score.Name = "high_score";
-            this.high_score.Size = new System.Drawing.Size(80, 17);
+            this.high_score.Size = new System.Drawing.Size(61, 13);
             this.high_score.TabIndex = 10;
             this.high_score.Text = "High score:";
             // 
             // new_game
             // 
-            this.new_game.Location = new System.Drawing.Point(49, 373);
+            this.new_game.Location = new System.Drawing.Point(37, 303);
+            this.new_game.Margin = new System.Windows.Forms.Padding(2);
             this.new_game.Name = "new_game";
-            this.new_game.Size = new System.Drawing.Size(106, 34);
+            this.new_game.Size = new System.Drawing.Size(80, 28);
             this.new_game.TabIndex = 11;
             this.new_game.Text = "New game";
             this.new_game.UseVisualStyleBackColor = true;
+            this.new_game.Click += new System.EventHandler(this.new_game_Click);
             // 
             // round_value
             // 
             this.round_value.AutoSize = true;
-            this.round_value.Location = new System.Drawing.Point(497, 43);
+            this.round_value.Location = new System.Drawing.Point(373, 35);
+            this.round_value.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.round_value.Name = "round_value";
-            this.round_value.Size = new System.Drawing.Size(46, 17);
+            this.round_value.Size = new System.Drawing.Size(13, 13);
             this.round_value.TabIndex = 12;
-            this.round_value.Text = "label1";
+            this.round_value.Text = "0";
             // 
             // high_score_value
             // 
             this.high_score_value.AutoSize = true;
-            this.high_score_value.Location = new System.Drawing.Point(522, 263);
+            this.high_score_value.Location = new System.Drawing.Point(392, 214);
+            this.high_score_value.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.high_score_value.Name = "high_score_value";
-            this.high_score_value.Size = new System.Drawing.Size(46, 17);
+            this.high_score_value.Size = new System.Drawing.Size(35, 13);
             this.high_score_value.TabIndex = 13;
             this.high_score_value.Text = "label1";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.high_score_value);
             this.Controls.Add(this.round_value);
             this.Controls.Add(this.new_game);
@@ -191,9 +205,9 @@ namespace minigame
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- minigame/minigame/Form1.cs
+++ minigame/minigame/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,17 +16,57 @@ namespace minigame
         public Form1()
         {
             InitializeComponent();
-            //dev branch test
+            List<Button> gombok = new List<Button>
+            {button1,button2,button3,button4,button5,button6,button7,button8,button9};
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+
+        private void new_game_Click(object sender, EventArgs e)
         {
+            //új játék
+            int kör = 0;
+            round_value.Text = kör.ToString();
 
+
+            for (int i = 0; i < 5; i++)
+            {
+                Random rnd = new Random();
+                int sorszam = rnd.Next(1, 10);
+                szinezo(sorszam);
+
+                Application.DoEvents();
+                Thread.Sleep(400);
+            }
+            
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        public void szinezo(int sorszam)
         {
+            //adott gombot átszínezi
+            if (sorszam == 1) button1.BackColor = Color.Red;
+            if (sorszam == 2) button2.BackColor = Color.Orange;
+            if (sorszam == 3) button3.BackColor = Color.Yellow;
+            if (sorszam == 4) button4.BackColor = Color.Lime;
+            if (sorszam == 5) button5.BackColor = Color.Aqua;
+            if (sorszam == 6) button6.BackColor = Color.Blue;
+            if (sorszam == 7) button7.BackColor = Color.Fuchsia;
+            if (sorszam == 8) button8.BackColor = Color.FromArgb(64,64,64);
+            if (sorszam == 9) button9.BackColor = Color.White;
+
+            Application.DoEvents();
+            Thread.Sleep(800);
+
+            button1.BackColor = Color.FromArgb(255, 128, 128);
+            button2.BackColor = Color.FromArgb(255, 192, 128);
+            button3.BackColor = Color.FromArgb(255, 255, 128);
+            button4.BackColor = Color.FromArgb(128, 255, 128);
+            button5.BackColor = Color.FromArgb(128, 255, 255);
+            button6.BackColor = Color.FromArgb(128, 128, 255);
+            button7.BackColor = Color.FromArgb(255, 128, 255);
+            button8.BackColor = Color.Gray;
+            button9.BackColor = Color.Silver;
 
         }
+
     }
 }
